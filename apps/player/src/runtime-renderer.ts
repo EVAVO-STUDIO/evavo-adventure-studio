6bbbf957d397a13a626527a881e532504d0f077e
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-30 02:06:23 +1000

    refactor: expose packaged renderer options for tests

--- apps/player/src/runtime-renderer.ts
+++ apps/player/src/runtime-renderer.ts
@@ -1,15 +1,23 @@
 import { createBitmapFontResolver } from "@evavo/adventure-bitmap-font/render";
-import { PixiWebGLRenderer } from "@evavo/adventure-renderer-pixi";
+import {
+  PixiWebGLRenderer,
+  type PixiRendererOptions,
+} from "@evavo/adventure-renderer-pixi";
 import type { PixiAssetTextureStore } from "@evavo/adventure-renderer-pixi/texture-store";
 import type { RuntimeBundle } from "@evavo/adventure-runtime-bundle";
+
+export const createPackagedRendererOptions = (
+  bundle: RuntimeBundle,
+  textures: PixiAssetTextureStore,
+): PixiRendererOptions => ({
+  textures,
+  ...(bundle.bitmapFonts
+    ? { bitmapFonts: createBitmapFontResolver(bundle.bitmapFonts) }
+    : {}),
+});
 
 export const createPackagedRuntimeRenderer = (
   bundle: RuntimeBundle,
   textures: PixiAssetTextureStore,
 ): PixiWebGLRenderer =>
-  new PixiWebGLRenderer({
-    textures,
-    ...(bundle.bitmapFonts
-      ? { bitmapFonts: createBitmapFontResolver(bundle.bitmapFonts) }
-      : {}),
-  });
+  new PixiWebGLRenderer(createPackagedRendererOptions(bundle, textures));
