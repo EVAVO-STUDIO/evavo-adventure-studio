cbf2c57045b3b19cbfdd42db498616c3ee804614
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-24 05:59:35 +1000

    feat(fidelity): register Police Quest I VGA remake pack

--- packages/adventure-design/src/reference-fidelity-presets.ts
+++ packages/adventure-design/src/reference-fidelity-presets.ts
@@ -9,6 +9,7 @@ import { foaReferenceTitlePack } from "./reference-fidelity-foa.js";
 import { heartOfChinaReferenceTitlePack } from "./reference-fidelity-heart-of-china.js";
 import { gk1ReferenceTitlePack } from "./reference-fidelity-gk1.js";
 import { kq5ReferenceTitlePack } from "./reference-fidelity-kq5.js";
+import { pq1VgaReferenceTitlePack } from "./reference-fidelity-pq1-vga.js";
 import { pq4ReferenceTitlePack } from "./reference-fidelity-pq4.js";
 import { qfg4ReferenceTitlePack } from "./reference-fidelity-qfg4.js";
 import { riseOfTheDragonReferenceTitlePack } from "./reference-fidelity-rise-of-the-dragon.js";
@@ -19,6 +20,7 @@ export const adventureReferenceTitlePacks: readonly AdventureReferenceTitlePack[
   kq5ReferenceTitlePack,
   qfg4ReferenceTitlePack,
   gk1ReferenceTitlePack,
+  pq1VgaReferenceTitlePack,
   pq4ReferenceTitlePack,
   foaReferenceTitlePack,
   heartOfChinaReferenceTitlePack,
