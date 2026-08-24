b87358900bcad2eb9829029139aed34a0c84cf4f
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-24 18:37:25 +1000

    Route root runtime controller through feature stack

--- packages/runtime-controller/src/index.ts
+++ packages/runtime-controller/src/index.ts
@@ -1,2 +1,10 @@
-export * from "./packaged-controller.js";
+export type {
+  PackagedRuntimeController,
+  PackagedRuntimeControllerOptions,
+} from "./packaged-controller.js";
+export {
+  createPackagedFeatureRuntimeController as createPackagedRuntimeController,
+  createPackagedFeatureSessionController,
+  describePackagedFeatureSession,
+} from "./feature-session.js";
 export * from "./profiled-camera.js";
