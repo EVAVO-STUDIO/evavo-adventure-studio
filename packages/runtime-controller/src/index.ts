6f6988d55975fdd983568fe704ede7d96eb050d0
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-24 18:36:21 +1000

    Make runtime controller root feature-aware

--- packages/runtime-controller/src/index.ts
+++ packages/runtime-controller/src/index.ts
@@ -1,2 +1,11 @@
-export * from "./packaged-controller.js";
+export type {
+  PackagedRuntimeController,
+  PackagedRuntimeControllerOptions,
+} from "./packaged-controller.js";
+export {
+  createPackagedFeatureSessionController as createPackagedRuntimeController,
+  describePackagedFeatureSession,
+  type PackagedFeatureSessionController,
+  type PackagedFeatureSessionDescription,
+} from "./feature-session.js";
 export * from "./profiled-camera.js";
