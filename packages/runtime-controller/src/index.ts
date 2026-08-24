3c18577a8c4c29e43fc53969299d8dde5e605789
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-24 18:36:37 +1000

    Restore base runtime controller root contract

--- packages/runtime-controller/src/index.ts
+++ packages/runtime-controller/src/index.ts
@@ -1,11 +1,2 @@
-export type {
-  PackagedRuntimeController,
-  PackagedRuntimeControllerOptions,
-} from "./packaged-controller.js";
-export {
-  createPackagedFeatureSessionController as createPackagedRuntimeController,
-  describePackagedFeatureSession,
-  type PackagedFeatureSessionController,
-  type PackagedFeatureSessionDescription,
-} from "./feature-session.js";
+export * from "./packaged-controller.js";
 export * from "./profiled-camera.js";
