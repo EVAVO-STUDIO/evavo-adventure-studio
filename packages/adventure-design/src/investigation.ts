0ee8320106a1eab4090f7a391cb4167d7a7d7db0
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-24 14:17:17 +1000

    Export Red Ledger runtime investigation source

--- packages/adventure-design/src/investigation.ts
+++ packages/adventure-design/src/investigation.ts
@@ -1,3 +1,4 @@
 export * from "./investigation-kernel.js";
 export * from "./investigation-analysis.js";
 export * from "./red-ledger-investigation-proof.js";
+export * from "./red-ledger-investigation-runtime-source.js";
