bf4b6233dc8b756725d97a76a5a29458a170e18c
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-25 12:49:52 +1000

    Export creative admission manifest

--- packages/adventure-design/src/creative-production.ts
+++ packages/adventure-design/src/creative-production.ts
@@ -1,4 +1,5 @@
 export * from "./creative-production-handoff.js";
 export * from "./creative-production-session.js";
 export * from "./creative-production-batch.js";
+export * from "./creative-production-admission.js";
 export * from "./ninth-reliquary-creative-production.js";
