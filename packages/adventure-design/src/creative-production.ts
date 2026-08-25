c6d222e37775ac4e8d67f0c73f23f761a068d053
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-25 12:48:40 +1000

    Export iterative creative production lifecycle

--- packages/adventure-design/src/creative-production.ts
+++ packages/adventure-design/src/creative-production.ts
@@ -1,2 +1,4 @@
 export * from "./creative-production-handoff.js";
+export * from "./creative-production-session.js";
+export * from "./creative-production-batch.js";
 export * from "./ninth-reliquary-creative-production.js";
